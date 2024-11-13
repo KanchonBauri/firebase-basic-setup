d53c1e9169e7c1f4c436b9e2872670e65ccbb82d
KanchonBauri <kanchonbauri1998@gmail.com>
2024-11-13 09:43:59 +0600

    github accoutn login

--- src/Comronents/Login/Login.jsx
+++ src/Comronents/Login/Login.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { GoogleAuthProvider, signInWithPopup,  } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup,  } from "firebase/auth";
 import auth from '../../Firebase/firebase';
 
 const Login = () => {
     const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
     const handleGoogleLogin = () =>{
         signInWithPopup(auth, googleProvider )
         .then(result => console.log(result))
     }
+
+    const handleGithubLogin = () =>{
+        signInWithPopup(auth, githubProvider )
+        .then(result => console.log(result))
+    }
     return (
         <div>
-           <button onClick={handleGoogleLogin} className='btn'>Gpoogle Login</button>
+           <button onClick={handleGoogleLogin} className='btn mx-5'>Gpoogle Login</button>
+           <button onClick={handleGithubLogin} className='btn'>Github Login</button>
         </div>
     );
 };
