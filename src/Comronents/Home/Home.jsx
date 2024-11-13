17833330f199a947afb24cf7cc837a8a1d00aaf5
KanchonBauri <kanchonbauri1998@gmail.com>
2024-11-13 14:55:33 +0600

    login to home skep code

--- src/Comronents/Home/Home.jsx
+++ src/Comronents/Home/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, TwitterAuthProvider,  } from "firebase/auth";
+import auth from '../../Firebase/firebase';
+
 
 const Home = () => {
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+    const twiterProvider = new TwitterAuthProvider();
+
+
+    const handleGoogleLogin = () =>{
+        signInWithPopup(auth, googleProvider )
+        .then(result => console.log(result))
+    }
+
+    const handleGithubLogin = () =>{
+        signInWithPopup(auth, githubProvider )
+        .then(result => console.log(result))
+    }
+
+    const handleTwiterLogin = () =>{
+        signInWithPopup(auth, twiterProvider)
+        .then(result => console.log(result))
+    }
+
     return (
         <div>
-            Home
+           <button onClick={handleGoogleLogin} className='btn mx-5'>Gpoogle Login</button>
+           <button onClick={handleGithubLogin} className='btn mx-5'>Github Login</button>
+           <button onClick={handleTwiterLogin} className='btn'>Github Login</button>
         </div>
     );
 };
--- src/Comronents/Login/Login.jsx
+++ src/Comronents/Login/Login.jsx
@@ -1,32 +1,9 @@
 import React from 'react';
-import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, TwitterAuthProvider,  } from "firebase/auth";
-import auth from '../../Firebase/firebase';
 
 const Login = () => {
-    const googleProvider = new GoogleAuthProvider();
-    const githubProvider = new GithubAuthProvider();
-    const twiterProvider = new TwitterAuthProvider();
-
-    const handleGoogleLogin = () =>{
-        signInWithPopup(auth, googleProvider )
-        .then(result => console.log(result))
-    }
-
-    const handleGithubLogin = () =>{
-        signInWithPopup(auth, githubProvider )
-        .then(result => console.log(result))
-    }
-
-    const handleTwiterLogin = () =>{
-        signInWithPopup(auth, twiterProvider)
-        .then(result => console.log(result))
-    }
-
     return (
         <div>
-           <button onClick={handleGoogleLogin} className='btn mx-5'>Gpoogle Login</button>
-           <button onClick={handleGithubLogin} className='btn mx-5'>Github Login</button>
-           <button onClick={handleTwiterLogin} className='btn'>Github Login</button>
+            navbar
         </div>
     );
 };
